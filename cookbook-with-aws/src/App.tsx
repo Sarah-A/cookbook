6098de4b7d6990b34e9471958f8ca2cefcde419f
SarahA <ashri.sarah@gmail.com>
2021-07-26 10:55:01 +1000

    Connect to AWS Backend using Amplify and Add User Authentication using Cognito
    Follwing turorial:  https://www.freecodecamp.org/news/ultimate-guide-to-aws-amplify-and-reacxt/

--- cookbook-with-aws/src/App.tsx
+++ cookbook-with-aws/src/App.tsx
@@ -1,26 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import "./App.css";
+
+Amplify.configure(awsconfig);
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        <h1>Welcome to my app!!</h1>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
