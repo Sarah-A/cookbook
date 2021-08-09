50fd05cfac1db85659d1ab00905218f33b87489c
SarahA <ashri.sarah@gmail.com>
2021-08-09 11:44:38 +1000

    Add front-end component to get the recipes using graphQL query

--- cookbook-with-aws/src/App.tsx
+++ cookbook-with-aws/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import "./App.css";
+import Recipes from "./Recipes";
 
 Amplify.configure(awsconfig);
 
@@ -11,8 +13,13 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <AmplifySignOut />
-        <h1>Welcome to my app!!</h1>
+        <h1>Cookbook</h1>
       </header>
+       <Router>
+         <Switch>
+           <Route path="/" component={Recipes} />             
+         </Switch>
+       </Router>
     </div>
   );
 }
