758ee0ab7b81d15f665f977d29153a9ef91de365
SarahA <ashri.sarah@gmail.com>
2021-08-18 12:28:16 +1000

    connected front end app to the AWS Backend via Amplify
    Notes:
    * This version successfully retrieve the recipes from the backend
    * I still need to add all the queries and mutations to the front end
    * I need to add support for authentication and login.

--- cookbook-with-aws-cdk/frontend/cookbook-react-app/src/App.tsx
+++ cookbook-with-aws-cdk/frontend/cookbook-react-app/src/App.tsx
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import { ReactElement } from 'react';
 import './App.css';
+import Recipes from "./components/Recipes";
 
-function App(){
+function App(): ReactElement {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Recipes />        
       </header>
     </div>
   );
---
+++ cookbook-with-aws-cdk/frontend/cookbook-react-app/src/components/Recipes.tsx
@@ -0,0 +1,41 @@
+import { FunctionComponent } from "react";
+import API, { graphqlOperation } from '@aws-amplify/api';
+import { useEffect } from 'react';
+
+const GetAllRecipes = `query getAllRecipes {
+    getAllRecipes {
+      id
+      name
+      description
+      ingredients
+      method
+      serves
+    }
+}`;
+
+
+async function getAllRecipes() {
+    console.log("Before calling API");
+    try {
+        const allRecipes = await API.graphql(graphqlOperation(GetAllRecipes));
+        console.log("After calling API");
+        console.log(allRecipes);    
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const Recipes : FunctionComponent = () => {
+
+    useEffect(() => {
+        void getAllRecipes();
+    }, [])
+
+    return (
+        <div>
+            Queried Recipes!!
+        </div>
+    );
+}
+
+export default Recipes;
