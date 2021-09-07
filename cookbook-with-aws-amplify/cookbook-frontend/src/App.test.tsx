cb82e0802ac18da050be36a6c24ab527abbc7dd9
SarahA <ashri.sarah@gmail.com>
2021-09-07 11:41:02 +1000

    starting from scratch after resetting AWS account

--- cookbook-with-aws-amplify/cookbook-frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const cookbookHeader = screen.getByText(/CookBook/i);
-  expect(cookbookHeader).toBeInTheDocument();
-});
