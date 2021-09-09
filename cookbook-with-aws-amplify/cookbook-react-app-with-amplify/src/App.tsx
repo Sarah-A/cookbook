30231f4670bd4365017017963dfd6d6a6d392a93
SarahA <ashri.sarah@gmail.com>
2021-09-09 10:31:55 +1000

    add sign-in and connect to amplify backend

--- cookbook-with-aws-amplify/cookbook-react-app-with-amplify/src/App.tsx
+++ cookbook-with-aws-amplify/cookbook-react-app-with-amplify/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignIn, AmplifySignOut, AmplifySignUp } from '@aws-amplify/ui-react';
+import { signInFields, signUpFields } from './Amplify/CustomAmplifyFields';
+import awsmobile from './aws-exports';
 
-function App() {
+Amplify.configure(awsmobile);
+
+function App() : ReactElement {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AmplifyAuthenticator>
+        <AmplifySignUp slot="sign-up" formFields={signUpFields} />
+        <AmplifySignIn slot="sign-in" formFields={signInFields} />
+
+        <header className="App-header">
+          <AmplifySignOut />
+          <h1>CookBook</h1>
+        </header>      
+        <main>     
+          <h2>This is where my main screen goes</h2>   
+        </main>
+      </AmplifyAuthenticator>
     </div>
   );
 }
