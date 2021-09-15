8e653d198187a8d84f4707132372f8df58bf0a3c
SarahA <ashri.sarah@gmail.com>
2021-09-15 13:05:42 +1000

    mocking experiments - WIP

---
+++ cookbook-with-aws-amplify/cookbook-react-app-with-amplify/src/components/__mocks__/Recipe.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+
+const Recipe: FunctionComponent = () => {
+
+    // TODO: Add a link to the recipe details (once I add them)
+    console.log(`In Recipe Mock!`);
+
+    return(
+        <div data-testid="recipe-mock" />
+    );
+
+}
+
+export default Recipe;
